adb67f04115d9ad1812adc94b9e715a9cdad37e4
Pappmichel <pappmichel@gmail.com>
2026-09-06 17:07:25 +0000

    fix: missing fillMaxWidth import broke Android CI build
    AssetOptimizedPlannerScreen.kt's LinearProgressIndicator used
    Modifier.fillMaxWidth() without importing
    androidx.compose.foundation.layout.fillMaxWidth, failing
    compileDebugKotlin on the last two pushes.

--- android/app/src/main/kotlin/com/pappmichel/evetraderlocal/ui/screens/AssetOptimizedPlannerScreen.kt
+++ android/app/src/main/kotlin/com/pappmichel/evetraderlocal/ui/screens/AssetOptimizedPlannerScreen.kt
@@ -2,6 +2,7 @@ package com.pappmichel.evetraderlocal.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
